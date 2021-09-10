791252eb5314ce58851efcb2687985cac270e2ff
monipeny <mlamasrial@gmail.com>
2021-09-10 10:05:12 +0200

    fetch

--- src/js/main.js
+++ src/js/main.js
@@ -1,3 +1,30 @@
 'use strict';
 
-console.log('>> Ready :)');
+const results = document.querySelector('.js_results');
+const input = document.querySelector('.js_input');
+const btn = document.querySelector('.js_btn');
+
+
+
+
+function handleSearch(event) {
+    event.preventDefault();
+    let search = input.value;
+    let dataResults = [];
+
+    fetch(`http://api.tvmaze.com/search/shows?q=${search}`)
+        .then(response => response.json())
+        .then((data) => {
+            console.log(data);
+            let title = document.querySelector('.js_title');
+            title.innerHTML = data[i].name;
+            let img = document.createElement('img');
+            document.body.append(img);
+            img.style = 'width: 160px';
+            img.src = data.image.medium;
+            img.alt = `Foto de ${title}`;
+        })
+
+}
+
+btn.addEventListener('click', handleSearch);
