551fd8019eb01f54fdfb377571c42eb33d4e66d5
monipeny <mlamasrial@gmail.com>
2021-09-10 14:16:47 +0200

    render search

--- src/js/main.js
+++ src/js/main.js
@@ -5,29 +5,45 @@ const input = document.querySelector('.js_input');
 const btn = document.querySelector('.js_btn');
 const showResults = document.querySelector('.js_showResults');
 
+let dataSeries = [];
+
+function getFromApi() {
+    let search = input.value;
+    fetch(`http://api.tvmaze.com/search/shows?q=${search}`)
+        .then(response => response.json())
+        .then((data) => {
+            dataSeries = data;
+            renderSearch();
+        })
+}
+
+function renderSearch() {
+    for (let i = 0; i < dataSeries.length; i++) {
+        const newItem = document.createElement('li');
+        const newTitle = document.createElement('h4');
+        const img = document.createElement('img');
+        showResults.appendChild(newItem);
+        newTitle.innerHTML = dataSeries[i].show.name;
+        newItem.appendChild(newTitle);
+        newItem.appendChild(img);
+        img.src = dataSeries[i].show.image.medium;
+        img.style = 'width: 160px';
+        img.alt = `Imagen de ${dataSeries[i].show.name}`;
+
+        // if (dataSeries[i].show.image === null) {
+        //     return img.src = 'https://via.placeholder.com/160';
+        // } else {
+        //     return img.src = dataSeries[i].show.image.medium;
+        // }
+    }
+}
+
 
 
 function handleSearch(event) {
     event.preventDefault();
-    let search = input.value;
+    getFromApi();
 
-    fetch(`http://api.tvmaze.com/search/shows?q=${search}`)
-        .then(response => response.json())
-        .then((data) => {
-            for (let i = 0; i < data.length; i++) {
-                const newItem = document.createElement('li');
-                const newTitle = document.createElement('h4');
-                newTitle.innerHTML = data[i].show.name;
-                newItem.appendChild(newTitle);
-                showResults.appendChild(newItem);
-                let img = document.createElement('img');
-                newItem.appendChild(img);
-                img.style = 'width: 160px';
-                img.src = data[i].show.image.medium;
-                img.alt = `Imagen de ${data[i].show.name}`;
-            }
-
-        })
 
 }
 
