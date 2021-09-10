8e9507ba2a32c6bfb05278f8ba91c18211745418
monipeny <mlamasrial@gmail.com>
2021-09-10 12:40:39 +0200

    bucle, createElement and appendChild

--- src/js/main.js
+++ src/js/main.js
@@ -3,29 +3,33 @@
 const results = document.querySelector('.js_results');
 const input = document.querySelector('.js_input');
 const btn = document.querySelector('.js_btn');
-
+const showResults = document.querySelector('.js_showResults');
 
 
 
 function handleSearch(event) {
     event.preventDefault();
     let search = input.value;
-    let dataResults = [];
 
     fetch(`http://api.tvmaze.com/search/shows?q=${search}`)
         .then(response => response.json())
         .then((data) => {
-            console.log(data);
-            console.log(data[0].show.name);
-            let title = document.querySelector('.js_title');
-            title.innerHTML = data[0].show.name;
-            let img = document.createElement('img');
-            document.body.append(img);
-            img.style = 'width: 160px';
-            img.src = data[0].show.image.medium;
-            img.alt = `Foto de ${title}`;
+            for (let i = 0; i < data.length; i++) {
+                const newItem = document.createElement('li');
+                const newTitle = document.createElement('h4');
+                newTitle.innerHTML = data[i].show.name;
+                newItem.appendChild(newTitle);
+                showResults.appendChild(newItem);
+                let img = document.createElement('img');
+                newItem.appendChild(img);
+                img.style = 'width: 160px';
+                img.src = data[i].show.image.medium;
+                img.alt = `Imagen de ${data[i].show.name}`;
+            }
+
         })
 
 }
+
 
 btn.addEventListener('click', handleSearch);
